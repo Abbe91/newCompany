29f57d320746e69dd9d75ae9b9923b87cce33338
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2024-10-24 20:40:55 +0200

    update the links

--- src/utils/links/internalLinks.js
+++ src/utils/links/internalLinks.js
@@ -1,292 +1,47 @@
 export const internallinks = [
   {
-    path: "tomma-dodsbo-goteborg",
-    title: "Tömma dödsbo Göteborg",
+    path: "digitalmarknadsforing-stockholm",
+    title: "Digital Marknadsföring Stockholm",
   },
   {
-    path: "koper-dodsbo-goteborg",
-    title: "Köper dödsbo Göteborg",
+    path: "digitalmarknadsforing-goteborg",
+    title: "Digital Marknadsföring Göteborg",
   },
   {
-    path: "uppkop-dodsbo-goteborg",
-    title: "Uppköp dödsbo Göteborg",
+    path: "digitalmarknadsforing-malmo",
+    title: "Digital Marknadsföring Malmö",
   },
-  {
-    path: "salja-dodsbo-goteborg",
-    title: "Sälja dödsbo Göteborg",
-  },
-  {
-    path: "vardera-dodsbo-goteborg",
-    title: "Värdera dödsbo Göteborg",
-  },
-  {
-    path: "hjalp-dodsbo-goteborg",
-    title: "Hjälp dödsbo Göteborg",
-  },
-  {
-    path: "bortforsling-dodsbo-goteborg",
-    title: "Bortforsling dödsbo Göteborg",
-  },
-  {
-    path: "dodsbo-goteborg",
-    title: "Dödsbo Göteborg",
-  },
-  {
-    path: "vardera-dodsbo-hisingen",
-    title: "Värdera dödsbo Hisingen",
-  },
-  {
-    path: "tomma-dodsbo-hisingen",
-    title: "Tömma dödsbo Hisingen",
-  },
-  {
-    path: "salja-dodsbo-hisingen",
-    title: "Sälja dödsbo Hisingen",
-  },
-  {
-    path: "uppkop-dodsbo-hisingen",
-    title: "Uppköp dödsbo Hisingen",
-  },
-  {
-    path: "bortforsling-dodsbo-hisingen",
-    title: "Bortforsling dödsbo Hisingen",
-  },
-  {
-    path: "uppkop-av-bohag",
-    title: "Uppköp av bohag",
-  },
-  {
-    path: "uppkop-av-dodsbo",
-    title: "Uppköp av dödsbo",
-  },
-  {
-    path: "roja-dodsbo-goteborg",
-    title: "Röja dödsbo Göteborg",
-  },
-  {
-    path: "stada-dodsbo-goteborg",
-    title: "Städa dödsbo Göteborg",
-  },
-  {
-    path: "bortforsling-dodsbo-partille",
-    title: "Bortforsling dödsbo Partille",
-  },
-  {
-    path: "salja-dodsbo-partille",
-    title: "Sälja dödsbo Partille",
-  },
-  {
-    path: "kopa-dodsbo-partille",
-    title: "Köpa dödsbo Partille",
-  },
-  {
-    path: "rojer-dodsbo-partille",
-    title: "Röjer dödsbo Partille",
-  },
-  {
-    path: "stada-dodsbo-partille",
-    title: "Städa dödsbo Partille",
-  },
-  {
-    path: "uppkop-dodsbo-partille",
-    title: "Uppköp dödsbo Partille",
-  },
+  // Add more links as needed
 ];
 
 export const popularLinks = [
   {
-    path: "vardera-dodsbo-ulricehamn",
-    title: "Värdera dödsbo Ulricehamn",
+    path: "digitalmarknadsforing-uppsala",
+    title: "Digital Marknadsföring Uppsala",
   },
   {
-    path: "hjalp-dodsbo-ulricehamn",
-    title: "Hjälp dödsbo Ulricehamn",
+    path: "digitalmarknadsforing-vasteras",
+    title: "Digital Marknadsföring Västerås",
   },
   {
-    path: "bortforsling-dodsbo-ulricehamn",
-    title: "Bortforsling dödsbo Ulricehamn",
+    path: "digitalmarknadsforing-orebro",
+    title: "Digital Marknadsföring Örebro",
   },
-  {
-    path: "salja-dodsbo-ulricehamn",
-    title: "Sälja dödsbo Ulricehamn",
-  },
-  {
-    path: "tomma-dodsbo-ulricehamn",
-    title: "Tömma dödsbo Ulricehamn",
-  },
-  {
-    path: "uppkop-dodsbo-ulricehamn",
-    title: "Uppköp dödsbo Ulricehamn",
-  },
-  {
-    path: "uppkop-dodsbo-ale",
-    title: "Uppköp dödsbo Ale",
-  },
-  {
-    path: "bortforsling-dodsbo-ale",
-    title: "Bortforsling dödsbo Ale",
-  },
-  {
-    path: "salja-dodsbo-ale",
-    title: "Sälja dödsbo Ale",
-  },
-  {
-    path: "vardera-dodsbo-ale",
-    title: "Värdera dödsbo Ale",
-  },
-  {
-    path: "tomma-dodsbo-molndal",
-    title: "Tömma dödsbo Mölndal",
-  },
-  {
-    path: "bortforsling-dodsbo-molndal",
-    title: "Bortforsling dödsbo Mölndal",
-  },
-  {
-    path: "dodsbo-firma-molndal",
-    title: "Dödsbo firma Mölndal",
-  },
-  {
-    path: "kopa-dodsbo-molndal",
-    title: "Köpa dödsbo Mölndal",
-  },
-  {
-    path: "roja-dodsbo-molndal",
-    title: "Röja dödsbo Mölndal",
-  },
-  {
-    path: "salja-dodsbo-molndal",
-    title: "Sälja dödsbo Mölndal",
-  },
-  {
-    path: "stada-dodsbo-molndal",
-    title: "Städa dödsbo Mölndal",
-  },
-  {
-    path: "bortforsling-dodsbo-stenungsund",
-    title: "Bortforsling dödsbo Stenungsund",
-  },
-  {
-    path: "roja-dodsbo-stenungsund",
-    title: "Röja dödsbo Stenungsund",
-  },
-  {
-    path: "kopa-dodsbo-stenungsund",
-    title: "Köpa dödsbo Stenungsund",
-  },
-  {
-    path: "salja-dodsbo-stenungsund",
-    title: "Sälja dödsbo Stenungsund",
-  },
-  {
-    path: "tomma-dodsbo-stenungsund",
-    title: "Tömma dödsbo Stenungsund",
-  },
-  {
-    path: "vardera-dodsbo-stenungsund",
-    title: "Värdera dödsbo Stenungsund",
-  },
+  // Add more links as needed
 ];
 
 export const fleromrodenLinks = [
   {
-    path: "flyttfirma/flyttfirma-goteborg",
-    title: "Flyttfirma Göteborg",
+    path: "digitalmarknadsforing-linkoping",
+    title: "Digital Marknadsföring Linköping",
   },
   {
-    path: "flyttfirma/flyttfirma-kungalv",
-    title: "Flyttfirma Kungälv",
+    path: "digitalmarknadsforing-helsingborg",
+    title: "Digital Marknadsföring Helsingborg",
   },
   {
-    path: "flyttfirma/billig-flyttfirma-goteborg",
-    title: "Billig flyttfirma Göteborg",
+    path: "digitalmarknadsforing-jonkoping",
+    title: "Digital Marknadsföring Jönköping",
   },
-  {
-    path: "flyttfirma/flyttfirma-offert",
-    title: "Flyttfirma Offert",
-  },
-  {
-    path: "tommer-hushall",
-    title: "Tömmer hushåll",
-  },
-  {
-    path: "tommer-hem",
-    title: "Tömmer hem",
-  },
-  {
-    path: "tommer-hem-goteborg",
-    title: "Tömmer hem i Göteborg",
-  },
-  {
-    path: "tommer-hushall-goteborg",
-    title: "Tömmer hushåll i Göteborg",
-  },
-  {
-    path: "vardera-dodsbo-kungalv",
-    title: "Värdera dödsbo Kungälv",
-  },
-  {
-    path: "hjalp-dodsbo-kungalv",
-    title: "Hjälp dödsbo Kungälv",
-  },
-  {
-    path: "bortforsling-dodsbo-kungalv",
-    title: "Bortforsling dödsbo Kungälv",
-  },
-  {
-    path: "salja-dodsbo-kungalv",
-    title: "Sälja dödsbo Kungälv",
-  },
-  {
-    path: "tomma-dodsbo-kungalv",
-    title: "Tömma dödsbo Kungälv",
-  },
-  {
-    path: "uppkop-dodsbo-kungalv",
-    title: "Uppköp dödsbo Kungälv",
-  },
-  {
-    path: "dodsbo-uppkopare-goteborg",
-    title: "Dödsbo uppköpare Göteborg",
-  },
-  {
-    path: "tommer-dodsbo",
-    title: "Tömmer dödsbo",
-  },
-  {
-    path: "dodsbofirma",
-    title: "Dödsbo firma",
-  },
-  {
-    path: "tomma-dodsbo",
-    title: "Tömma Dödsbo",
-  },
-  {
-    path: "tomma-dodsbo-ale",
-    title: "Tömma dödsbo Ale",
-  },
-  {
-    path: "dodsbo-uppkopare",
-    title: "Dödsbo uppköpare",
-  },
-  {
-    path: "foretag-som-koper-bohag",
-    title: "Företag som köper bohag",
-  },
-  {
-    path: "vardering-av-dodsbo",
-    title: "Värdering av dödsbo",
-  },
-  {
-    path: "flyttstadning",
-    title: "Flyttstädning",
-  },
-  {
-    path: "flyttstadning-i-kungalv",
-    title: "Flyttstädning i Kungälv",
-  },
-  {
-    path: "/webbdesign",
-    title: "webbdesign",
-  },
+  // Add more links as needed
 ];
